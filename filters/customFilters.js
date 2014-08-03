3e8fd1640cd9584bd5942e6e202a10cc5ff351b3
Niraj Pant <nirajpant7@gmail.com>
2014-08-03 17:03:04 -0500

    new filter + implement in html

--- filters/customFilters.js
+++ filters/customFilters.js
@@ -33,4 +33,17 @@ return function (data, propertyName) {
     }
   }
 })
+.filter("pageCount", function() {
+  return function (data, size) {
+    if (angular.isArray(data)) {
+      var result = [];
+      for (var i = 0; i < Math.ceil(data.length / size); i++) {
+        result.push(i);
+      }
+      return result;
+    } else {
+      return data;
+    }
+  }
+})
 });
