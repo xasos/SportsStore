b0570131088addb6f972e13a9c640267633d906c
Niraj Pant <nirajpant7@gmail.com>
2014-08-03 18:09:04 -0500

    bugs were squashed

--- controllers/productListControllers.js
+++ controllers/productListControllers.js
@@ -1,31 +1,33 @@
 angular.module("sportsStore")
-.constant("productListActiveClass", "btn-primary")
-.controller("productListPageCount", 3)
-.controller("productListCtrl", function($scope, $filter, productListActiveClass) {
+    .constant("productListActiveClass", "btn-primary")
+    .constant("productListPageCount", 3)
+    .controller("productListCtrl", function ($scope, $filter,
+        productListActiveClass, productListPageCount) {
 
-var selectedCatergory = null;
-$scope.selectedPage = 1;
-$scope.pageSize = productListPageCount;
+        var selectedCategory = null;
 
-$scope.selectCategory = function (newCategory) {
-  selectedCategory = newCategory;
-  $scope.selectedPage = 1;
-}
+        $scope.selectedPage = 1;
+        $scope.pageSize = productListPageCount;
 
-$scope.selectPage = function(newPage) {
-  $scope.selectedPage = newPage;
-}
+        $scope.selectCategory = function (newCategory) {
+            selectedCategory = newCategory;
+            $scope.selectedPage = 1;
+        }
 
-$scope.categoryFilterFn = function (product) {
-  return selectedCategory == null ||
-      product.category == selectedCategory;
-}
+        $scope.selectPage = function (newPage) {
+            $scope.selectedPage = newPage;
+        }
 
-$scope.getCategoryClass = function (category) {
-  return selectedCategory == category ? productListActiveClass : "";
-}
+        $scope.categoryFilterFn = function (product) {
+            return selectedCategory == null ||
+                product.category == selectedCategory;
+        }
 
-$scope.getPageClass = function (page) {
-  return $scope.selectedPage == page ? productListActiveClass : "";
-}
-});
+        $scope.getCategoryClass = function (category) {
+            return selectedCategory == category ? productListActiveClass : "";
+        }
+
+        $scope.getPageClass = function (page) {
+            return $scope.selectedPage == page ? productListActiveClass : "";
+        }
+    });
