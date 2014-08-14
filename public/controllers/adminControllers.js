bf7f90c8810649353307e7c674778e883ec796b3
Niraj Pant <nirajpant7@gmail.com>
2014-08-13 21:10:34 -0500

    Added order + products views

--- public/controllers/adminControllers.js
+++ public/controllers/adminControllers.js
@@ -1,6 +1,7 @@
 angular.module("sportsStoreAdmin")
 .constant("authUrl", "http://localhost:5500/users/login")
 .controller("authCtrl", function($scope, $http, $location, authUrl) {
+
     $scope.authenticate = function (user, pass) {
       $http.post(authUrl, {
       username: user,
@@ -13,3 +14,17 @@ angular.module("sportsStoreAdmin")
         $scope.authenticationError = error;
     });
 })
+.controller("mainCtrl", function($scope) {
+
+    $scope.screens = ["Products", "Orders"];
+    $scope.current = $scope.screens[0];
+
+    $scope.setScreen = function (index) {
+        $scope.current = $scope.screens[index];
+    }
+
+    $scope.getScreen = function() {
+        return $scope.current == "Products" ? "/views/adminProducts.html" : "/views/adminOrders.html";
+
+    };
+});
